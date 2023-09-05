1fe91a8cb7a46dec0f0fe837450e327b0ea35a41
TelesphoreUwabera <t.uwabera@alustudent.com>
2023-09-05 09:11:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,42 @@
 import "./style.css";
 
-// Get references to the counter elements
 const counterElement = document.querySelector('.counter');
 const incrementButton = document.querySelector('.increment');
 const decrementButton = document.querySelector('.decrement');
 
-// Initialize the counter value
 let counterValue = 0;
 
-// Function to update the counter display
 function updateCounterDisplay() {
   counterElement.textContent = counterValue;
 }
 
-// Function to handle the increment button click
 function handleIncrementClick() {
-  counterValue += 1; // Increase by 1
+  counterValue++;
   updateCounterDisplay();
 }
 
-// Function to handle the decrement button click
 function handleDecrementClick() {
-  counterValue -= 1; // Decrease by 1
+  counterValue--;
   updateCounterDisplay();
 }
 
-// Add click event listeners to the buttons
-incrementButton.addEventListener('click', handleIncrementClick);
-decrementButton.addEventListener('click', handleDecrementClick);
+function handleIncrementMouseOver() {
+  incrementButton.addEventListener('click', handleIncrementClick);
+}
 
-// Initial update of the counter display
+
+function handleDecrementMouseOver() {
+  decrementButton.addEventListener('click', handleDecrementClick);
+}
+
+function handleMouseLeave() {
+  incrementButton.removeEventListener('click', handleIncrementClick);
+  decrementButton.removeEventListener('click', handleDecrementClick);
+}
+
+incrementButton.addEventListener('mouseover', handleIncrementMouseOver);
+decrementButton.addEventListener('mouseover', handleDecrementMouseOver);
+incrementButton.addEventListener('mouseleave', handleMouseLeave);
+decrementButton.addEventListener('mouseleave', handleMouseLeave);
+
 updateCounterDisplay();
